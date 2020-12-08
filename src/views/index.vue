d3359e03e0780165cfedb01e748937b2e79de002
ru xia <641533508@qq.com>
2020-12-08 10:03:24 +0800

    1

---
+++ src/views/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="cart">
+      <div class="hearderbox">
+          <header class="w">
+              <div class="w-box">
+                  <div class="nav-logo">
+                      <h1>
+                          <a href="javascript:;" class="router-link-active" title="XMall商城官网">XMall商城</a>
+                      </h1>
+                  </div>
+                  <div class="right-box">
+                      <div class="nav-list">
+                          <div class="el-complete">
+                             <div class="el-input">
+                                 <i class="el-input-search"></i>
+                                 <input type="text" class="el-input-inner" placeholder="请输入商品信息">
+                             </div>
+                             <a href="javascript:;">
+                                 <a>全部商品</a>
+                             </a>
+                              <a href="javascript:;">
+                                 <a>捐赠</a>
+                             </a>
+                          </div>
+                      </div>
+                      <div class="nav-aside">
+                          <div class="user">
+                             <a href="javascript:;"></a>
+                          </div>
+                          <div class="shop">
+                             <a href="javascript:;"></a>
+                          </div>
+
+                      </div> 
+                  </div>
+              </div>
+          </header>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
