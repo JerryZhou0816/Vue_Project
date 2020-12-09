e7c0f4639d7c3466738a2ed1c70ee6839073dab1
Zhouh <zhouhao@zhouhaodeMacBook-Pro.local>
2020-12-09 11:53:45 +0800

    提交后台文件,api,请求拦截器

--- src/main.js
+++ src/main.js
@@ -14,10 +14,15 @@ import "swiper/dist/css/swiper.min.css";
 import VueLazyload from "vue-lazyload";
 Vue.use(VueLazyload);
 
+// 引入api
+import * as Http from "@/api";
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  beforeCreate() {
+    Vue.prototype.$API = Http;
+  },
   render: (h) => h(App),
 }).$mount("#app");
